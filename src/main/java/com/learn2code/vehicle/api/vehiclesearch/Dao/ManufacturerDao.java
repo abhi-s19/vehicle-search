441b4660e62a444874ea2bc4dd17842e7105161d
Abhisek Garai <abhi.garai.2090@gmail.com>
2024-02-16 22:18:51 +0530

    created manufacturerservice and manufacturyearserviceimpl and rename manufacturerdao

--- src/main/java/com/learn2code/vehicle/api/vehiclesearch/Dao/ManufacturerDao.java
+++ src/main/java/com/learn2code/vehicle/api/vehiclesearch/Dao/ManufacturerDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learn2code.vehicle.api.vehiclesearch.entity.Manufacturer;
 
-public interface ManufacturDao extends JpaRepository<Manufacturer, Integer> {
+public interface ManufacturerDao extends JpaRepository<Manufacturer, Integer> {
 
 }
---
+++ src/main/java/com/learn2code/vehicle/api/vehiclesearch/serviceimpl/ManufacturerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.learn2code.vehicle.api.vehiclesearch.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learn2code.vehicle.api.vehiclesearch.Dao.ManufacturerDao;
+import com.learn2code.vehicle.api.vehiclesearch.entity.Manufacturer;
+import com.learn2code.vehicle.api.vehiclesearch.service.ManufacturerService;
+
+@Service
+public class ManufacturerServiceImpl implements ManufacturerService {
+	
+	@Autowired
+	private ManufacturerDao manufacturerDao;
+	@Override
+	public Manufacturer saveManufacturer(Manufacturer manufacturer) {
+		// TODO Auto-generated method stub
+		return manufacturerDao.save(manufacturer);
+	}
+
+}
