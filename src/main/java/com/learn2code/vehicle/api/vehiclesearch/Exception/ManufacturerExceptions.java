5c332c3f8829d0a731d088d4405d8fc83ce4bb4b
Abhisek Garai <abhi.garai.2090@gmail.com>
2024-02-22 22:18:17 +0530

    MethodArgumentNotValidException

--- src/main/java/com/learn2code/vehicle/api/vehiclesearch/Exception/ManufacturerExceptions.java
+++ src/main/java/com/learn2code/vehicle/api/vehiclesearch/Exception/ManufacturerExceptions.java
@@ -2,6 +2,7 @@ package com.learn2code.vehicle.api.vehiclesearch.Exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +17,15 @@ public class ManufacturerExceptions {
 		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND,ex.getMessage());
 		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.NOT_FOUND);
 		
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		
+		ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST,ex.getFieldError().getDefaultMessage());
+		return new ResponseEntity<ErrorResponse>(errorResponse,HttpStatus.BAD_REQUEST);
+	   
 	}
 	
 }
